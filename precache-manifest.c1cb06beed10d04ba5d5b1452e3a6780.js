89c032890702be23a8d32d5e695d154ed115b14d
Ketan <ketantoo@gmail.com>
2020-01-07 19:57:21 +0530

    Updates

---
+++ precache-manifest.c1cb06beed10d04ba5d5b1452e3a6780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a7476eac686441ffa04452cde7b113",
+    "url": "/YouTubeApp/index.html"
+  },
+  {
+    "revision": "dfe3954e797f6d8f918d",
+    "url": "/YouTubeApp/static/css/main.9f64764d.chunk.css"
+  },
+  {
+    "revision": "f7780f2d4f3bf26f6b7d",
+    "url": "/YouTubeApp/static/js/2.24e023da.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/YouTubeApp/static/js/2.24e023da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfe3954e797f6d8f918d",
+    "url": "/YouTubeApp/static/js/main.17c396ad.chunk.js"
+  },
+  {
+    "revision": "00c6452784f86c1f4554",
+    "url": "/YouTubeApp/static/js/runtime-main.058bf828.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtubeapp/precache-manifest.4f8dc3b8197fbc9511b341ab77a57017.js"
+  "/YouTubeApp/precache-manifest.c1cb06beed10d04ba5d5b1452e3a6780.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtubeapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YouTubeApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
